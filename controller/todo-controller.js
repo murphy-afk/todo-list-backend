0365c0669e3877e5ff43b1f5c3577b30944c4932
murphy-afk <bardarenoemi@gmail.com>
2026-02-19 15:17:59 +0100

    feat: index query change

--- controller/todo-controller.js
+++ controller/todo-controller.js
@@ -1,7 +1,7 @@
 import connection from "../data/db.js";
 
 function index(req, res) {
-  const query = 'SELECT * FROM todos';
+  const query = 'SELECT todos.id, todos.title, todos.description, todos.deadline, todos.completed, priorities.name as priority FROM todos INNER JOIN priorities ON todos.priority_id = priorities.id';
 
   connection.query(query, (err, results) => {
     if (err) return res.status(500).json({ error: 'database query failed'});
